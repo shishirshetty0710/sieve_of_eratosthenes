73ad304b613325030774ee196b4b88664c04d7bd
Shishir <shishirshetty0710@gmail.com>
2017-10-21 00:18:08 -0400

    Add a new class SieveNum. Features added in code.

--- src/sieveoferatosthenes/SieveOfEratosthenes.java
+++ src/sieveoferatosthenes/SieveOfEratosthenes.java
@@ -16,6 +16,44 @@ public class SieveOfEratosthenes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
     }
-    
+
+}
+
+/**
+ * Class SieveNum will store the number and a flag value to check if this number
+ * was flagged as not a prime number in any iteration..
+ */
+class SieveNum {
+
+    int num;
+    boolean flag;
+
+    public SieveNum(int num, boolean flag) {
+        this.num = num;
+        this.flag = flag;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public String toString() {
+        return "SieveNum{" + "num=" + num + ", flag=" + flag + '}';
+    }
+
 }
