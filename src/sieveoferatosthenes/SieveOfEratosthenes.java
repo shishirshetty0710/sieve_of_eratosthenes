c0fe9dbd1d0f1e998fc2279756b58d5908f0827c
Shishir <shishirshetty0710@gmail.com>
2017-10-21 01:41:05 -0400

    Completed the logic. But not efficient.

--- src/sieveoferatosthenes/SieveOfEratosthenes.java
+++ src/sieveoferatosthenes/SieveOfEratosthenes.java
@@ -5,6 +5,7 @@
  */
 package sieveoferatosthenes;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,22 +21,54 @@ public class SieveOfEratosthenes {
         // TODO code application logic here
 
         /*Take input from user*/
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Enter the number: ");
-       int n = scanner.nextInt();
-       System.out.println("Prime numbers from 0 to "+n);
-       new SieveOfEratosthenes().calculatePrimeNum(n);
-       
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = scanner.nextInt();
+        System.out.println("Prime numbers from 0 to " + n);
+        new SieveOfEratosthenes().calculatePrimeNum(n);
+
     }
-    
+
     /*
       Method to displayFirst set of prime numbers
-    */
-    void calculatePrimeNum(int num){
-    
-        System.out.println(num);
-        
-    
+     */
+    void calculatePrimeNum(int num) {
+
+        num += 1;
+        ArrayList<SieveNum> numbers = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+
+            numbers.add(new SieveNum(i, false));
+
+        }
+
+        int pos = 2;
+        while (pos < num) {
+
+            if (!numbers.get(pos).isFlag()) {
+                for (int i = pos; i < numbers.size(); i++) {
+
+                    SieveNum number = numbers.get(i);
+
+                    if (number.getNum() != pos && number.getNum() % pos == 0) {
+                        
+                        number.setFlag(true);
+                    }
+                }
+            }
+
+            pos++;
+        }
+
+        for (SieveNum numObj : numbers) {
+
+            if (!numObj.isFlag()) {
+                System.out.println(numObj.getNum());
+            }
+        }
+      
+
     }
 
 }
